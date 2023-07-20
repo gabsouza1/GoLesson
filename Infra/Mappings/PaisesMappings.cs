a9eca2396e2518c19d8001c949961d52104fd050
GABRIEL GUSTAVO DE SOUZA <201920328@unifoa.edu.br>
2023-07-20 20:22:13 -0300

    Exemplificado Interfaces, Repositorios, Mappings e DataContext

---
+++ Infra/Mappings/PaisesMappings.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+using Domain.Entities;
+
+namespace Infra.Mappings
+{
+    public class PaisesMapping : IEntityTypeConfiguration<Paises>
+    {
+        public void Configure(EntityTypeBuilder<Paises> builder)
+        {
+            builder.ToTable("Paises");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.NomePais).HasMaxLength(255).IsRequired();
+        }
+    }
+}
