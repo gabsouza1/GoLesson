4fbdcf18f7faf4a17590b5c161215f3f7242da2e
GABRIEL GUSTAVO DE SOUZA <201920328@unifoa.edu.br>
2023-07-20 20:29:33 -0300

    Adicionado Repository do Pais

---
+++ Infra/Repositories/PaisesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Infra.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositories
+{
+    public class PaisesRepository : Repository<Paises>
+    {
+        public PaisesRepository(DataContext dataContext) : base(dataContext)
+        {
+
+        }
+    }
+}
