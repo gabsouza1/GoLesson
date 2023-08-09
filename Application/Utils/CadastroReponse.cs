de8fd098765cd4aef40930105df3402771a522a8
GABRIEL GUSTAVO DE SOUZA <201920328@unifoa.edu.br>
2023-08-08 21:44:24 -0300

    Criação de validações de cadastro de usuário

---
+++ Application/Utils/CadastroReponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Utils
+{
+    public class CadastroReponse
+    {
+        public bool Sucesso { get; set; }
+        public List<string> Erros { get; set; }
+
+
+        public CadastroReponse() =>  Erros = new List<string>();
+
+        public CadastroReponse(bool sucesso = true) : this() => Sucesso = sucesso;
+
+        public void AdicionarErros(List<string> erros) =>
+            Erros.AddRange(erros);
+    }
+}
---
+++ Application/ViewModels/CadastroViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels
+{
+    public class CadastroViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string SobreNome { get; set; }
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} é invalido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [Compare(nameof(Email), ErrorMessage = "Email não coincidem")]
+        public string ConfirmarEmail { get; set; }
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [PasswordPropertyText]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "O campo {0} obrigatório")]
+        [Compare(nameof(Senha), ErrorMessage = "Senhas não coincidem")]
+        [PasswordPropertyText]
+        public string ConfirmarSenha { get; set; }
+    }
+}
