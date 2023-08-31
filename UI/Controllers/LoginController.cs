5721268e799713ca79d4fbbe7619c1a7f920c52d
GABRIEL GUSTAVO DE SOUZA <gabriel.souza.gs2@csn.com.br>
2023-08-31 13:16:52 -0300

    Implementação do metodo de login

--- UI/Controllers/LoginController.cs
+++ UI/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Application.ViewModels;
+using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,13 +9,46 @@ namespace UI.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
-        public LoginController()
+        private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly ILogger<Usuario> _logger;
+        public LoginController(UserManager<Usuario> userManager, ILogger<Usuario> logger, SignInManager<Usuario> signInManager)
         {
+            _logger = logger;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            try 
+            {
+                if(ModelState.IsValid) 
+                {
+                    var isAuthenticated = await _signInManager.PasswordSignInAsync(model.Email, model.Senha, model.LembrarDeMim, false);
+
+                    if(isAuthenticated.Succeeded) 
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Email ou Senha incorretos");
+                    }
+                }
+                return View(model); 
+            }catch (Exception ex)
+            {
+                return View("Index");
+            }
+        }
+
+
+
 
 
 
